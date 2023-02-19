5e299a0f5ee587292d80644223d9b62f5a2032d0
Sergio Zella <sergioluiz.zella@gmail.com>
2023-02-19 10:16:53 -0300

    Primeiro commit

---
+++ src/main/java/br/com/szella/intranetcondominial/IntranetCondominialApplication.java
@@ -0,0 +1,13 @@
+package br.com.szella.intranetcondominial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetCondominialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntranetCondominialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/szella/intranetcondominial/IntranetCondominialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.szella.intranetcondominial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetCondominialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
