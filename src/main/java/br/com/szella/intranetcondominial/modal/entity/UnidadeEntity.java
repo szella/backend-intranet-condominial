3424399c892912fe502260be630a34849a3ab7bb
Sergio Zella <sergioluiz.zella@gmail.com>
2023-06-12 22:17:46 -0300

    Ajustes de classes

--- src/main/java/br/com/szella/intranetcondominial/modal/entity/UnidadeEntity.java
+++ src/main/java/br/com/szella/intranetcondominial/modal/entity/UnidadeEntity.java
@@ -1,7 +1,19 @@
 package br.com.szella.intranetcondominial.modal.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.NaturalIdCache;
